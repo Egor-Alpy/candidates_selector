4b3f0dbbfaf479cfb081082c8530ea5bc6cbf9e2
alpy <wndrflp@yandex.ru>
2025-08-24 18:15:00 +0300

    add: query

--- app/core/es_settings/queries.py
+++ app/core/es_settings/queries.py
@@ -1,6 +1,40 @@
+from typing import Optional
+
+
 class ElasticQueries:
     """Агрегация поисковоых запросов для ElasticSearch"""
 
     @staticmethod
-    def search_foo(search_query: str):
-        return {"че_искать": search_query}
+    def search_es_v1(position_title: str, yandex_category: Optional[str] = '', offset: Optional[int]=100):
+        return {
+            "query": {
+                "bool": {
+                    "must": [
+                        {
+                            "match": {
+                                "title": {
+                                    "query": position_title,
+                                    "operator": "or",
+                                }
+                            }
+                        },
+                        {
+                            "bool": {
+                                "should": [
+                                    {"term": {"yandex_category.exact": yandex_category}},
+                                    {
+                                        "match": {
+                                            "yandex_category": {
+                                                "query": yandex_category,
+                                                "operator": "or",
+                                            }
+                                        }
+                                    },
+                                ]
+                            }
+                        },
+                    ]
+                }
+            },
+            "size": offset,
+        }
