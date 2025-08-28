90b07b2af1533f4e2c73a0d1368997f6f9d313be
alpy <wndrflp@yandex.ru>
2025-08-28 22:01:16 +0300

    update: queries

--- app/core/es_settings/queries.py
+++ app/core/es_settings/queries.py
@@ -1,11 +1,17 @@
 from typing import Optional
 
+from app.models.tenders import TenderPositions
+
 
 class ElasticQueries:
     """Агрегация поисковоых запросов для ElasticSearch"""
 
     @staticmethod
-    def get_query_v1(position_title: str, yandex_category: Optional[str] = '', size: Optional[int]=100):
+    def get_query_v1(
+        position_title: str,
+        yandex_category: Optional[str] = "",
+        size: Optional[int] = 100,
+    ):
         return {
             "query": {
                 "bool": {
@@ -21,7 +27,11 @@ class ElasticQueries:
                         {
                             "bool": {
                                 "should": [
-                                    {"term": {"yandex_category.exact": yandex_category}},
+                                    {
+                                        "term": {
+                                            "yandex_category.exact": yandex_category
+                                        }
+                                    },
                                     {
                                         "match": {
                                             "yandex_category": {
@@ -40,7 +50,11 @@ class ElasticQueries:
         }
 
     @staticmethod
-    def get_query_v1_shrink_enums(position_info: dict, yandex_category: Optional[str] = '', size: Optional[int]=100):
+    def get_query_v1_shrink_enums(
+        position_info: dict,
+        yandex_category: Optional[str] = "",
+        size: Optional[int] = 100,
+    ):
         return {
             "query": {
                 "bool": {
@@ -48,7 +62,7 @@ class ElasticQueries:
                         {
                             "match": {
                                 "title": {
-                                    "query": position_info['name'],
+                                    "query": position_info["name"],
                                     "operator": "or",
                                 }
                             }
@@ -56,7 +70,11 @@ class ElasticQueries:
                         {
                             "bool": {
                                 "should": [
-                                    {"term": {"yandex_category.exact": yandex_category}},
+                                    {
+                                        "term": {
+                                            "yandex_category.exact": yandex_category
+                                        }
+                                    },
                                     {
                                         "match": {
                                             "yandex_category": {
@@ -75,36 +93,42 @@ class ElasticQueries:
         }
 
     @staticmethod
-    def get_query_v2(position_title: str, yandex_category: Optional[str] = '', size: Optional[int]=100):
+    def get_query_v2(
+        position_title: str,
+        yandex_category: Optional[str] = "",
+        size: Optional[int] = 100,
+    ):
         return {
             "query": {
-              "bool": {
-                "should": [
-                  {
-                    "bool": {
-                      "must": [
-                        {"match": {"title": position_title}},
-                        {"term": {"yandex_category.exact": yandex_category}}
-                      ],
-                      "boost": 3
-                    }
-                  },
-                  {
-                    "match": {
-                      "title": {
-                        "query": position_title,
-                        "boost": 1
-                      }
-                    }
-                  }
-                ],
-                "minimum_should_match": 1
-              }
+                "bool": {
+                    "should": [
+                        {
+                            "bool": {
+                                "must": [
+                                    {"match": {"title": position_title}},
+                                    {
+                                        "term": {
+                                            "yandex_category.exact": yandex_category
+                                        }
+                                    },
+                                ],
+                                "boost": 3,
+                            }
+                        },
+                        {"match": {"title": {"query": position_title, "boost": 1}}},
+                    ],
+                    "minimum_should_match": 1,
+                }
             },
             "size": size,
         }
+
     @staticmethod
-    def get_query_for_rabbit(position_title: str, yandex_category: Optional[str] = ''):
+    def get_query_for_rabbit(
+        position_title: str,
+        yandex_category: Optional[str] = "",
+        size: Optional[int] = 10000,
+    ):
         return {
             "query": {
                 "bool": {
@@ -120,7 +144,11 @@ class ElasticQueries:
                         {
                             "bool": {
                                 "should": [
-                                    {"term": {"yandex_category.exact": yandex_category}},
+                                    {
+                                        "term": {
+                                            "yandex_category.exact": yandex_category
+                                        }
+                                    },
                                     {
                                         "match": {
                                             "yandex_category": {
@@ -135,4 +163,82 @@ class ElasticQueries:
                     ]
                 }
             },
+            "size": size,
         }
+
+    @staticmethod
+    def get_query_v5(position: TenderPositions, size: Optional[int] = 10000):
+        """"""
+        # razbor positon data title = position_data['title'] i t.d.
+
+        positions_demands = []
+        from app.core.logger import get_logger
+        logger = get_logger(name=__name__)
+
+        # Поиск по обычным полям документа
+        positions_demands.append(
+            {
+                "multi_match": {
+                    "query": position.title,
+                    "fields": [
+                        "title^3",
+                        "title.ngram^2",
+                        "category^2",
+                        "category.ngram",
+                        "yandex_category^2",
+                        "yandex_category.ngram",
+                        "description^1.5",
+                        "description.ngram",
+                    ],
+                    "type": "best_fields",
+                    "fuzziness": "AUTO",
+                }
+            },
+        )
+        positions_demands.append(
+            {
+                "multi_match": {
+                    "query": position.category,
+                    "fields": [
+                        "category^2",
+                        "yandex_category^2",
+                    ],
+                    "type": "best_fields",
+                }
+            },
+        )
+
+        for attribute in position.attributes:
+            continue
+            if attribute.type != "Количественная" and attribute.type != "Диапазон":
+                positions_demands.append(
+                    {
+                        "nested": {
+                            "path": "attributes",
+                            "query": {
+                                "multi_match": {
+                                    "query": attribute.value,
+                                    "fields": [
+                                        "attributes.attr_name^2",
+                                        "attributes.attr_name.ngram",
+                                        "attributes.attr_value^3",
+                                        "attributes.attr_value.ngram^1.5",
+                                    ],
+                                    "type": "best_fields",
+                                    "fuzziness": "AUTO",
+                                }
+                            },
+                        }
+                    }
+)
+
+        query = {
+            "query": {
+                "bool": {
+                    "must": positions_demands
+                }
+            },
+            "size": size,
+        }
+
+        return query
