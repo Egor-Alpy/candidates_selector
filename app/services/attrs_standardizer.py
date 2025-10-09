18e37648351f9a8bb524de73c7ac9071da1d8dd4
alpy <wndrflp@yandex.ru>
2025-10-09 19:25:52 +0300

    add: retry logic for attr standardizer

--- app/services/attrs_standardizer.py
+++ app/services/attrs_standardizer.py
@@ -12,18 +12,23 @@ class AttrsStandardizer:
         self.api_url = api_url
 
     async def extract_attr_data(self, string_to_handle: str):
-        try:
-            session = await connection_pool.get_http_session('attrs_standardizer')
-            url = f"{self.api_url}/standardize"
-            payload = [string_to_handle]
+        for attempt in range(1, 4):
+            try:
+                session = await connection_pool.get_http_session('attrs_standardizer')
+                url = f"{self.api_url}/standardize"
+                payload = [string_to_handle]
 
-            async with session.post(url, json=payload) as response:
-                if response.status == 200:
-                    result = await response.json()
-                    return result
-                else:
-                    return None
+                async with session.post(url, json=payload) as response:
+                    if response.status == 200:
+                        if attempt > 1:
+                            logger.warning(f"Ответ получен с {attempt}-й попытки!")
+                        result = await response.json()
+                        return result
+                    else:
+                        logger.error(f"Ошибка, статус-код = {response.status}, response: {response.json()}")
+                        return None
 
-        except Exception as e:
-            logging.error(f"Ошибка при вычленении сущностей из названия и значения характеристики: {e}")
-            return None
+            except Exception as e:
+                logger.error(f"Ошибка при вычленении сущностей из названия и значения характеристики, попытка {attempt}: {e}")
+                continue
+        return None
