0fc297192049e1fc35c7f70feb63351726baf008
alpy <wndrflp@yandex.ru>
2025-10-11 15:46:24 +0300

    remove: lemmatization and stemming logic (moved to another preprocessing service - elastic absorber)

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import health, compare, lemmatiztion_and_stemming
+from app.api.v1.endpoints import health, compare
 
 api_router = APIRouter(prefix='/v1', tags=['v1'])
 
-routers = [health.router, compare.router, lemmatiztion_and_stemming.router]
+routers = [health.router, compare.router]
 
 # Подключаем роутеры
 for router in routers:
